b951eb2d1be26783fc79b8294703fdb898c5f433
Capixp <warnerxp@gmail.com>
2025-11-09 22:54:24 -0600

    Prisma

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -7,7 +7,6 @@ async function getCategories() {
 }
 
 export default async function OrderSidebar() {
-  //const categories = await prisma.category.findMany();
   const categories = await getCategories();
   console.log(categories);
   return (
